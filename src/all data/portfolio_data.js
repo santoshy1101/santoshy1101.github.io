97ea0374cdbf87c8c13628f7679da3d55c476d98
santosh <styadav018@gmail.com>
2023-05-21 04:49:12 +0530

    added port by vite

---
+++ src/all data/portfolio_data.js
@@ -0,0 +1,186 @@
+import fashionPool from "../assets/portfolio/fashionPool.png";
+import moviesMunch from "../assets/portfolio/moviesMunch.png";
+import textBoard from "../assets/portfolio/textBoard.png";
+import asos from "../assets/portfolio/wearly.png";
+import reactParallax from "../assets/portfolio/reactParallax.jpg";
+import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import reactImage from "../assets/react.png";
+import chakra from "../assets/chakra.png";
+import node from "../assets/node.png";
+import sass from "../assets/sass.png";
+import redux from "../assets/redux.png";
+import reactIcons from "../assets/reactIcons.png";
+
+ export const portfolio_data = [
+   {
+      id: 1,
+      src: moviesMunch,
+      title:"MoviesMunch",
+      demo: "https://movies-munch.vercel.app/",
+      code: "https://github.com/santoshy1101/Movies-Munch",
+      desc:"The MoviesMunch web app will provide information about movies and their trailers. It will include the latest movie releases, trailers, and detailed information about each movie. ",
+      tech_stacks:[
+        {
+          id: 4,
+          src: reactImage,
+          title: "REACT",
+          style: "shadow-blue-600",
+        },
+      {
+        id: 2,
+        src: sass,
+        title: "SASS",
+        style: "shadow-pink-500",
+      },
+      {
+        id: 3,
+        src:redux,
+        title: "REDUX",
+        style: "shadow-violet-500",
+      },
+      {
+        id: 4,
+        src:reactIcons,
+        title: "REACTICONS",
+        style: "shadow-red-500",
+      },
+      ,]
+    },
+    {
+      id: 2,
+      title:"Mytheresa",
+      src: fashionPool,
+      demo:"https://fashionpool.netlify.app/",
+      code:"https://github.com/santoshy1101/Fashion-Pool-Mythersa-s-Clone",
+      desc:"FashionPool is an online clothing store featuring stylish and affordable apparel for all occasions. Our selection of clothing caters to all ages, sizes and styles.",
+      tech_stacks:[
+        {
+        id: 1,
+        src: html,
+        title: "HTML",
+        style: "shadow-orange-500",
+      },
+      {
+        id: 2,
+        src: css,
+        title: "CSS",
+        style: "shadow-blue-500",
+      },
+      {
+        id: 3,
+        src: javascript,
+        title: "JavaScript",
+        style: "shadow-yellow-500",
+      },
+      {
+        id: 4,
+        src: reactImage,
+        title: "React",
+        style: "shadow-blue-600",
+      },]
+    },
+    // {
+    //   id: 2,
+    //   src: maxFashion,
+    //   title:"MaxFashion",
+    //   demo: "https://shiny-stardust-c87756.netlify.app/",
+    //   code: "https://github.com/santoshy1101/Max-Fashion-Clone",
+    //   desc:"Max Fashion is an online ecommerce platform offering a wide range of products from apparel, footwear, accessories and beauty products for men, women and kids.",
+    //   tech_stacks:[
+    //     {
+    //     id: 1,
+    //     src: html,
+    //     title: "HTML",
+    //     style: "shadow-orange-500",
+    //   },
+    //   {
+    //     id: 2,
+    //     src: chakra,
+    //     title: "Chakra Ui",
+    //     style: "shadow-green-500",
+    //   },
+    //   {
+    //     id: 3,
+    //     src: javascript,
+    //     title: "JavaScript",
+    //     style: "shadow-yellow-500",
+    //   },
+    //   {
+    //     id: 4,
+    //     src: reactImage,
+    //     title: "React",
+    //     style: "shadow-blue-600",
+    //   },]
+    // },
+    ,
+    {
+      id: 3,
+      src: asos,
+      title:"ASOS",
+      demo: "https://wearly.vercel.app/",
+      code: "https://github.com/santoshy1101/forlorn-cause-7661",
+      desc:"ASOS plc is a British online fashion and cosmetic retailer. The company was founded in 2000 in London, primarily aimed at young adults.",
+      tech_stacks:[
+        {
+        id: 1,
+        src: node,
+        title: "NODE",
+        style: "shadow-green-500",
+      },
+      {
+        id: 2,
+        src: chakra,
+        title: "Chakra Ui",
+        style: "shadow-green-500",
+      },
+      {
+        id: 3,
+        src: javascript,
+        title: "JavaScript",
+        style: "shadow-yellow-500",
+      },
+      {
+        id: 4,
+        src: reactImage,
+        title: "React",
+        style: "shadow-blue-600",
+      },]
+    },
+    {
+      id: 4,
+      src: textBoard,
+      title:"TextBoard",
+      demo: "https://text-board-0ab662.netlify.app/",
+      code: "https://github.com/santoshy1101/Text-Board-WebApp",
+      desc:"TextBoard is Kind of text editer where you can convert all of your paragraph lowerCase to upperCases and upperCase to lowerCase. You Can also remove extra space from the paragraph",
+      tech_stacks:[
+        {
+        id: 1,
+        src: html,
+        title: "HTML",
+        style: "shadow-orange-500",
+      },
+      {
+        id: 2,
+        src: chakra,
+        title: "Chakra Ui",
+        style: "shadow-green-500",
+      },
+      {
+        id: 3,
+        src: javascript,
+        title: "JavaScript",
+        style: "shadow-yellow-500",
+      },
+      {
+        id: 4,
+        src: reactImage,
+        title: "React",
+        style: "shadow-blue-600",
+      },]
+    }
+  ];
